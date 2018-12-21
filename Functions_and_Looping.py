355889b3d660d6abaecf9284be352c50e5ef4ce3
Sergey Kryukov <cydkom@gmail.com>
2018-12-21 23:37:13 +0300

    advice + advice2

--- Functions_and_Looping.py
+++ Functions_and_Looping.py
@@ -3,3 +3,15 @@ praise = praise.upper()
 number_of_characters = len(praise)
 result = praise + "!" * number_of_characters
 print(result)
+
+advice = "Don't forget to ask for help"
+advice = advice.upper()
+number_of_characters = len(advice)
+result = advice + "!" * number_of_characters
+print(result)
+
+advice2 = "Don't repeat Yourself. Keep things DRY!"
+advice2 = advice2.upper()
+number_of_characters = len(advice2)
+result = advice2 + "!" * number_of_characters
+print(result)
